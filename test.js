e3c105ae1c610d2cfeeea81312535c3b526ca260
manikeshwari54 <manikeshwari542006@gmail.com>
2025-11-19 21:58:12 +0530

    Fix module export and clean up test.js
    Removed package.json content from test.js and fixed export.

--- test.js
+++ test.js
@@ -6,22 +6,3 @@ app.get('/will', (req, res) => res.send('{ "response": "Hello World" }'));
 app.get('/ready', (req, res) => res.send('{ "response": "Great!, It works!" }'));
 
 module.exports = app;
-
-file 3 package.json
-
-{
-  "name": "node-docker-demo",
-  "version": "1.0.0",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js",
-    "test": "mocha test.js"
-  },
-  "dependencies": {
-    "express": "^4.18.2"
-  },
-  "devDependencies": {
-    "supertest": "^6.3.3",
-    "mocha": "^10.2.0"
-  }
-}
